24f74f828d57c7ab7a80ab991b73326e2a1bfec1
Judson James <judsonjames96@gmail.com>
2018-01-23 15:16:55 -0500

    Hello World Commit

---
+++ hello_world/hello_world.cc
@@ -0,0 +1,21 @@
+/*******************************************************************************
+* Author and Copyright: Judson James
+* For: CSCE 240, Spring 2018
+*******************************************************************************/
+
+// These are the includes and the namespace usage for "cout", which is our
+// "print"  function for C++. You can alternatively use "using namespace std;"
+// to replace both lines 12 and 13, however, the way I'm using it here is more
+// verbose and explicit.
+#include <iostream>
+#include <cstdlib>
+using std::cout;
+using std::endl;
+
+// This is a basic example of a main method, where the parameters are the
+// number of arguments provided at the command line along with the names of the
+// arguments provided.
+int main (int argc, char** argv) {
+  cout << "Hello World" << endl;
+  return 0;
+}
