0f761c2a9a9e43dbb2cfdcfa6e2692158cbace36
Brennan Cain <brenn.cain@gmail.com>
2018-02-07 17:27:22 -0500

    Added pip-based cpplint
    Prevents having to create a folder in the user's  home folder

--- useful_files/install_cpplint_linux.sh
+++ useful_files/install_cpplint_linux.sh
@@ -9,12 +9,15 @@
 #   - Assigns the command to the console as a shortcut
 #*******************************************************************************
 
-echo "Cloning the cpplint Repository from Github to root directory...\n"
-cd ~
-git clone https://github.com/google/styleguide
+if [ $(which pip) ] ; then 
+  echo "Installing cpplint from pip"
+  sudo pip install cpplint
+else
+  echo "Cloning the cpplint Repository from Github to root directory...\n"
+  cd ~
+  git clone https://github.com/google/styleguide
 
-echo "Assigning cpplint as a shortcut to the console...\n"
-echo "alias cpplint=\"python ~/styleguide/cpplint/cpplint.py\"" >> ~/.bashrc
-
+  echo "Assigning cpplint as a shortcut to the console...\n"
+  echo "alias cpplint=\"python ~/styleguide/cpplint/cpplint.py\"" >> ~/.bashrc
+fi
 echo "To use: cpplint <c++ file>\n"
-
